2b59e46adc72883c3e92f095b2da2d04aca7a738
Ananya Hari Narain <40917852+AnanyaHN27@users.noreply.github.com>
2020-08-03 18:18:11 +0100

    Update focs3-unit-tests.ml

--- backend/unit-tests/focs3-unit-tests.ml
+++ backend/unit-tests/focs3-unit-tests.ml
@@ -8,7 +8,7 @@ let rec isIn x lst = match lst with
 	| [] -> false
 	| hd::tl -> (hd==x) || (isIn x tl);;
 
-let ins_all_positions x l = 
+let insAllPositions x l = 
 	let rec aux prev acc = function
 	| [] -> (prev @ [x]) :: acc |> List.rev
 	| hd::tl as l -> aux (prev @ [hd]) ((prev @ [x] @ [l]) :: acc) tl in aux [] [] l;;
@@ -27,6 +27,69 @@ let testExfOnGivenExampleInQuestion = assert_equal ([2;8] || [8;2]) (Submission.
 let testExfOnLargeExample = 
 	let largeExampleList= intsTo 500 in
 	assert_equal (isIn perms (intsTo 498)) (Submission.exf (fun x-> x+2) largeExampleList)
+
+(*concatenating lazy lists: concat*)
+
+let testConcatGeneratesLazyList = 
+	let rec matchLazyList xs = match xs with
+	| Nil -> true
+	| Cons (_,_) -> true in
+	let rec from k = Cons (k, function () -> from (k+1)) in
+	let list1 = from 1 in
+	let list2 = from 2 in
+	assert_true matchLazyList (Submission.concat list1 list2) ;;
+
+(*let testConcatProperlyAppendsList2ToList1 =
+	let rec checkProperAppending xs ys = match xs, ys
+	let rec from k = Cons (k, function () -> from (k+1)) in
+	let list1 = from 1 in
+	let list2 = from 2 in
+	assert_true *) 
+	
+
+(*interleaving lazy lists: combine*)
+
+let testCombineGeneratesLazyList = 
+	let rec matchLazyList xs = match xs with
+	| Nil -> true
+	| Cons (_,_) -> true in
+	let rec from k = Cons (k, function () -> from (k+1)) in
+	let list1 = from 1 in
+	let list2 = from 2 in
+	assert_true matchLazyList (Submission.combine list1 list2);;
+
+(*all0s1s*)
+
+let testAll0s1sGeneratesLazyList = 
+	let rec matchLazyList xs = match xs with
+	| Nil -> true
+	| Cons (_,_) -> true in
+	assert_true matchLazyList (Submission.all0s1s);;
+
+let testAll0s1sEachElementInLazyListHasOnly0sand1s = 
+	let rec hasOnly1sAnd0s xs = match xs with 
+	| [] -> true 
+	| x::xs -> (x==1 || x=0) || hasOnly1sAnd0s xs in
+	let rec checkOnly1sAnd0s xs = match xs with
+	| Nil -> true
+	| Cons(a, b) -> if (hasOnly1sAnd0s a) then (checkOnly1sAnd0s (b())) else false in
+	assert_true checkOnly1sAnd0s (Submission.all0s1s);;
+	
+
+(*palindrome*)
+
+let testPalindromeGeneratesLazyList = 
+	let rec matchLazyList xs = match xs with
+	| Nil -> true
+	| Cons (_,_) -> true in
+	assert_true matchLazyList (Submission.palindrome);;
+
+let testEachElementInLazyListIsPalindromic = 
+	let isPalindrome xs = (xs = List.rev xs) in
+	let rec checkPalindromeLazyList xs = match xs with
+	| Nil -> true
+	| Cons (a, b) -> if (isPalindrome a) then (checkPalindromeLazyList (b())) else false in
+	assert_true checkPalindromeLazyList (Submission.palindrome);;
 
 let suite =
 "Focs3TestSuite">:::
